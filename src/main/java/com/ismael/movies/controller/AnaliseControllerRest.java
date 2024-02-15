33bccea921b31065e67ef7c951e65c26f8f7ccbf
Ismael Nunes Campos <ismaeldenunes@gmail.com>
2024-02-14 22:59:38 -0300

    [FIXED] endpoints mudados com o prefixo de api

--- src/main/java/com/ismael/movies/controller/AnaliseControllerRest.java
+++ src/main/java/com/ismael/movies/controller/AnaliseControllerRest.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/analise")
+@RequestMapping("api/v1/analise")
 public class AnaliseControllerRest {
 
     @Autowired
--- src/main/java/com/ismael/movies/controller/FilmeControllerRest.java
+++ src/main/java/com/ismael/movies/controller/FilmeControllerRest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/filme")
+@RequestMapping("api/v1/filme")
 public class FilmeControllerRest {
 
     @Autowired
