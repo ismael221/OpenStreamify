1e3a9b4be7adc3bd6e949913f64b563676aaae80
Ismael Nunes <ismaeldenunes@gmail.com>
2024-09-12 01:39:59 -0300

    [FIX] Creating the folders relative to the app instead of using the users folder path

--- src/main/java/com/ismael/movies/services/HlsService.java
+++ src/main/java/com/ismael/movies/services/HlsService.java
@@ -8,28 +8,38 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
 public class HlsService {
 
-    private final Path baseLocation  = Paths.get("videos/hls");
-    private  static  final Logger logger = LoggerFactory.getLogger(HlsService.class);
+    private final Path baseLocation = Paths.get("videos/hls");
 
+    private static final Logger logger = LoggerFactory.getLogger(HlsService.class);
+
+    public HlsService() {
+        // Cria o diretório "videos/hls" se ele não existir
+        try {
+            Files.createDirectories(baseLocation);
+        } catch (IOException e) {
+            logger.error("Could not create directory: " + baseLocation.toString(), e);
+            throw new RuntimeException("Could not create upload directory!");
+        }
+    }
 
     public Resource getHlsResource(String filename) throws MalformedURLException {
-        //Path movieFolderPath = baseLocation.resolve(movieFolder).normalize();
         Path filePath = baseLocation.resolve(filename).normalize();
         Resource resource = new UrlResource(filePath.toUri());
         if (resource.exists() && resource.isReadable()) {
             return resource;
         } else {
-            logger.error("Resource not found: "+filename);
+            logger.error("Resource not found: " + filename);
             throw new RuntimeException("Resource not found: " + filename);
         }
     }
-
 
 }
