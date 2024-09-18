dceb5d6561fa6d0055bb0a3d7ee4323e203b47b2
Ismael Nunes <ismaeldenunes@gmail.com>
2024-09-18 01:09:11 -0300

    [ADD] Adding docker files to run everything inside a container properly

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Usa a imagem do OpenJDK para o Spring Boot
+FROM openjdk:17-jdk-alpine
+
+RUN apk add --no-cache ffmpeg
+
+# Define o diretório de trabalho no container
+WORKDIR /app
+
+# Copia o jar da aplicação para o container
+COPY target/movie.jar /app/movie.jar
+
+# Expõe a porta em que a aplicação irá rodar
+EXPOSE 8080
+
+# Define o comando para iniciar a aplicação
+ENTRYPOINT ["java", "-jar", "movie.jar"]
---
+++ src/main/java/com/ismael/movies/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.ismael.movies.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        // ! You have to provide the redisStandaloneConfiguration or else the app wont
+        // ! work with docker
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+}
