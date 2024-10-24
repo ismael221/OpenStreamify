2f3c3cb60b5a54261c4b51d0e645eedb18566ead
Ismael Nunes <ismaeldenunes@gmail.com>
2024-10-24 08:07:05 -0300

    [ADD] OpenStreamify logo on console

--- src/main/java/com/ismael/movies/infra/security/SecurityConfiguration.java
+++ src/main/java/com/ismael/movies/infra/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -63,13 +64,12 @@ public class SecurityConfiguration {
                                 .defaultSuccessUrl("/")
                                 .permitAll()
                 )
+                .logout(logout -> logout
+                        .logoutUrl("/signout").permitAll()
+                )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .logout(logout -> logout
-                        .logoutUrl("/signout").permitAll()
-                        .logoutSuccessUrl("/signout").permitAll()
-                    )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
